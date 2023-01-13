8ea4eb9661c60bcc6cdcf0c26437a2bd72d52eb6
Dylan Depass <dylandepass@gmail.com>
2023-01-13 09:58:46 -0500

    fix: setup github release

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -9,6 +9,13 @@ module.exports = {
     ["@semantic-release/git", {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    ["@semantic-release/github", {
+      "assets": [
+        { "path": "styles/system/system.css", "label": "SDS Stylesheet", "name": "system.css" },
+        { "path": "brands/EECOL/theme.css", "label": "EECOL Theme (CSS)", "name": "eecol.css" },
+        { "path": "brands/EECOL/theme.scss", "label": "EECOL Theme (SCSS)", "name": "eecol.scss" },
+      ]
     }]
   ],
   branches: ['main'],

8c015d197bbcc7864cb65488d43487166e00f2f8
Dylan Depass <dylandepass@gmail.com>
2023-01-31 10:12:55 -0500

    fix: remove deploy to docs folder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 const path = require('path');
 module.exports = {
   "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../docs/**/*.mdx",
+    "../docs/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",

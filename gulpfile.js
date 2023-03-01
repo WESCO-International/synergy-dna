164d0cbec6e4daa6e24cd7dae95d6566b672c16a
Dylan Depass <dylandepass@gmail.com>
2023-03-01 13:26:25 -0500

    fix: remove gulp util and gulp plumber

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,13 @@ const {
   series, dest, src, watch,
 } = require('gulp');
 const postcss = require('gulp-postcss');
-const plumber = require('gulp-plumber');
-const gutil = require('gulp-util');
 const rename = require('gulp-rename');
 const header = require('gulp-header');
 
 const generatedHeader = '/* stylelint-disable */\n/* Generated File: Do not edit directly */\n';
 
-/**
- * UTIL TASKS
- */
-const errorMsg = '\x1b[41mError\x1b[0m';
-
-function onError(err) {
-  gutil.beep();
-  gutil.log(`${errorMsg} ${err.toString()}`);
-
-  // @ts-ignore
-  this.emit('end');
-}
-
 function buildSystem() {
   return src('./system/index.css')
-    .pipe(plumber({ errorHandler: onError }))
     .pipe(postcss())
     .pipe(header(generatedHeader))
     .pipe(rename('system.css'))
@@ -33,7 +17,6 @@ function buildSystem() {
 
 function buildStorybookCss() {
   return src('.storybook/story.post.css')
-    .pipe(plumber({ errorHandler: onError }))
     .pipe(postcss())
     .pipe(header(generatedHeader))
     .pipe(rename('story.css'))

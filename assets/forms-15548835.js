db0f017ee3869d6d35db5f7f9b5b8eca3d098055
dylandepass <dylandepass@gmail.com>
2023-03-02 21:01:40 +0000

    Deploying to gh-pages from  @ 76504965c411bf1d8462b9b1f79497c605a4251a 🚀

---
+++ assets/forms-15548835.js
@@ -0,0 +1,38 @@
+import{j as e}from"./jsx-runtime-ccada58e.js";import{M as d}from"./index-3820782f.js";import{T as o}from"./index-c4b7e6b1.js";import{t as i}from"./theme-85f03007.js";import{u as r}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-f8ff18ba.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-50df77d3.js";import"./chunk-XHUUYXNA-a398bfb0.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-3EFM6HRY-454626f5.js";import"./index-029c8be7.js";import"./_baseToString-30508fa8.js";import"./_getTag-5d4511eb.js";import"./isEqual-81c992b2.js";import"./index-1fe2e1f1.js";import"./uniq-efb824e2.js";import"./index-356e4a49.js";function t(s){const n=Object.assign({h1:"h1",blockquote:"blockquote",p:"p",h2:"h2"},r(),s.components);return e.jsxs(e.Fragment,{children:[e.jsx(d,{title:"Docs/Developers/Tokens/Form"}),`
+`,e.jsx(n.h1,{children:"Forms"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"The Synergy Design System provides CSS & mixins that is pre-configured to utilize the tokens below. They should not be used for any purpose other than the element or semantic meaning they where created for."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Form Tokens"}),`
+`,e.jsx(n.p,{children:"These tokens are apply to form group elements such as labels and Help text."}),`
+`,e.jsxs("div",{className:"table token",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(o,{tokens:i.component.form,prefix:"--sds-component-form"})]}),`
+`,e.jsx(n.h2,{children:"TextField Tokens"}),`
+`,e.jsx(n.p,{children:"These tokens are used for both text field and text areas."}),`
+`,e.jsxs("div",{className:"table token",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(o,{tokens:i.component["text-field"],prefix:"--sds-component-text-field"})]}),`
+`,e.jsx(n.h2,{children:"Checkbox Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(o,{tokens:i.component.checkbox,prefix:"--sds-component-checkbox"})]}),`
+`,e.jsx(n.h2,{children:"Switch Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(o,{tokens:i.component.switch,prefix:"--sds-component-switch"})]}),`
+`,e.jsx(n.h2,{children:"Radio Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(o,{tokens:i.component.radio,prefix:"--sds-component-radio"})]}),`
+`,e.jsx(n.h2,{children:"Select Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(o,{tokens:i.component.select,prefix:"--sds-component-select"})]})]})}function q(s={}){const{wrapper:n}=Object.assign({},r(),s.components);return n?e.jsx(n,Object.assign({},s,{children:e.jsx(t,s)})):t(s)}export{q as default};
+//# sourceMappingURL=forms-15548835.js.map
--- assets/icons-5540d380.js
+++
@@ -1,9 +0,0 @@
-import{j as i}from"./jsx-runtime-ccada58e.js";import{M as c}from"./index-3820782f.js";import{t as m}from"./theme-5cbe4d86.js";import{C as p}from"./index-c4b7e6b1.js";import{u as n}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-f8ff18ba.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-50df77d3.js";import"./chunk-XHUUYXNA-a398bfb0.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-3EFM6HRY-454626f5.js";import"./index-029c8be7.js";import"./_baseToString-30508fa8.js";import"./_getTag-5d4511eb.js";import"./isEqual-81c992b2.js";import"./index-1fe2e1f1.js";import"./uniq-efb824e2.js";import"./index-356e4a49.js";function t(o){const e=Object.assign({h1:"h1",p:"p",pre:"pre",code:"code"},n(),o.components);return i.jsxs(i.Fragment,{children:[i.jsx(c,{title:"Docs/Developers/Mixins/Icons"}),`
-`,i.jsx(e.h1,{children:"Icon Mixins"}),`
-`,i.jsx(e.p,{children:"Synergy provides the following mixin to work with icons."}),`
-`,i.jsx(e.pre,{children:i.jsx(e.code,{className:"language-css",children:`@mixin icon {size};
-`})}),`
-`,i.jsxs("div",{className:"table margin-top",children:[i.jsxs("div",{children:[i.jsx("div",{children:"Parameter"}),`
-`,i.jsx("div",{children:"Options"})]}),i.jsxs("div",{children:[i.jsx("div",{children:i.jsx(e.code,{children:"size"})}),`
-`,i.jsx("div",{children:Object.entries(m.semantic.size.icon).map(([r,s])=>i.jsx(p,{k:r,value:s,url:"/?path=/docs/docs-developers-tokens-icon--docs"}))})]})]})]})}function F(o={}){const{wrapper:e}=Object.assign({},n(),o.components);return e?i.jsx(e,Object.assign({},o,{children:i.jsx(t,o)})):t(o)}export{F as default};
-//# sourceMappingURL=icons-5540d380.js.map

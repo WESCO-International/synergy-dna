811f5299306ceeb9354fb05b755f6667399787be
jdilanwesco <105079415+jdilanwesco@users.noreply.github.com>
2023-03-06 16:49:00 +0000

    Deploying to gh-pages from  @ 48be987f7175bdfb201c5822fc8333065a65883c 🚀

---
+++ storybook/assets/border-388221e3.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-ccada58e.js";import{M as d}from"./index-3820782f.js";import{T as i}from"./index-c4b7e6b1.js";import{t as o}from"./theme-6bb2ed99.js";import{u as t}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-f8ff18ba.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-50df77d3.js";import"./chunk-XHUUYXNA-a398bfb0.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-3EFM6HRY-454626f5.js";import"./index-029c8be7.js";import"./_baseToString-30508fa8.js";import"./_getTag-5d4511eb.js";import"./isEqual-81c992b2.js";import"./index-1fe2e1f1.js";import"./uniq-efb824e2.js";import"./index-356e4a49.js";function s(n){const r=Object.assign({h1:"h1",blockquote:"blockquote",p:"p",h2:"h2"},t(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(d,{title:"Docs/Developers/Tokens/Border"}),`
+`,e.jsx(r.h1,{children:"Borders"}),`
+`,e.jsxs(r.blockquote,{children:[`
+`,e.jsx(r.p,{children:"The Synergy Design System provides CSS & mixins that is pre-configured to utilize the tokens below. They should not be used for any purpose other than the element or semantic meaning they where created for."}),`
+`]}),`
+`,e.jsx(r.h2,{children:"Border Radius Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:" "}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:o.semantic["border-radius"],prefix:"--sds-semantic-border-radius"})]}),`
+`,e.jsx(r.h2,{children:"Border Width Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:" "}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:o.semantic["border-width"],prefix:"--sds-semantic-border-width"})]}),`
+`,e.jsx(r.h2,{children:"Border Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(i,{tokens:o.semantic.color.border,prefix:"--sds-semantic-color-border"})]})]})}function O(n={}){const{wrapper:r}=Object.assign({},t(),n.components);return r?e.jsx(r,Object.assign({},n,{children:e.jsx(s,n)})):s(n)}export{O as default};
+//# sourceMappingURL=border-388221e3.js.map

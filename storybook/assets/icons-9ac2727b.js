ec0f2dcdc2e375ec77cf9413501ececf294df425
jdilanwesco <105079415+jdilanwesco@users.noreply.github.com>
2023-03-07 12:58:56 +0000

    Deploying to gh-pages from  @ a1f663eed0299652c38379314a2a3aceab798534 🚀

---
+++ storybook/assets/icons-9ac2727b.js
@@ -0,0 +1,11 @@
+import{j as e}from"./jsx-runtime-ccada58e.js";import{M as r}from"./index-3820782f.js";import{T as s}from"./index-c4b7e6b1.js";import{t as m}from"./theme-b87e8b72.js";import{u as i}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-f8ff18ba.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-50df77d3.js";import"./chunk-XHUUYXNA-a398bfb0.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-3EFM6HRY-454626f5.js";import"./index-029c8be7.js";import"./_baseToString-30508fa8.js";import"./_getTag-5d4511eb.js";import"./isEqual-81c992b2.js";import"./index-1fe2e1f1.js";import"./uniq-efb824e2.js";import"./index-356e4a49.js";function n(o){const t=Object.assign({h1:"h1",blockquote:"blockquote",p:"p",h2:"h2"},i(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"Docs/Developers/Tokens/Icon"}),`
+`,e.jsx(t.h1,{children:"Sizes"}),`
+`,e.jsxs(t.blockquote,{children:[`
+`,e.jsx(t.p,{children:"The Synergy Design System provides CSS & mixins that is pre-configured to utilize the tokens below. They should not be used for any purpose other than the element or semantic meaning they where created for."}),`
+`]}),`
+`,e.jsx(t.h2,{children:"Icon Size Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:" "}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:m.semantic.size.icon,prefix:"--sds-semantic-size-icon"})]})]})}function X(o={}){const{wrapper:t}=Object.assign({},i(),o.components);return t?e.jsx(t,Object.assign({},o,{children:e.jsx(n,o)})):n(o)}export{X as default};
+//# sourceMappingURL=icons-9ac2727b.js.map
---
+++ storybook/assets/size-b630dc13.js
@@ -0,0 +1,21 @@
+import{j as e}from"./jsx-runtime-ccada58e.js";import{M as m}from"./index-3820782f.js";import{a as s,T as a}from"./index-c4b7e6b1.js";import{t}from"./theme-b87e8b72.js";import{u as r}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-f8ff18ba.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-50df77d3.js";import"./chunk-XHUUYXNA-a398bfb0.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-3EFM6HRY-454626f5.js";import"./index-029c8be7.js";import"./_baseToString-30508fa8.js";import"./_getTag-5d4511eb.js";import"./isEqual-81c992b2.js";import"./index-1fe2e1f1.js";import"./uniq-efb824e2.js";import"./index-356e4a49.js";function o(i){const n=Object.assign({h1:"h1",blockquote:"blockquote",p:"p",h2:"h2"},r(),i.components);return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Docs/Developers/Tokens/Size"}),`
+`,e.jsx(n.h1,{children:"Sizes"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"The Synergy Design System provides CSS & mixins that is pre-configured to utilize the tokens below. They should not be used for any purpose other than the element or semantic meaning they where created for."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Section Size Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:" "}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{k:"max-width",value:t.semantic.size.section["max-width"],prefix:"--sds-semantic-size-section"})]}),`
+`,e.jsx(n.h2,{children:"Default Content Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:" "}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{k:"margin-bottom",value:t.semantic.size["default-content-wrapper"]["margin-bottom"],prefix:"--sds-semantic-size-default-content-wrapper"})]}),`
+`,e.jsx(n.h2,{children:"Breakpoint Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:" "}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(a,{tokens:t.semantic.size.breakpoints,prefix:"--sds-semantic-size-breakpoints"})]})]})}function V(i={}){const{wrapper:n}=Object.assign({},r(),i.components);return n?e.jsx(n,Object.assign({},i,{children:e.jsx(o,i)})):o(i)}export{V as default};
+//# sourceMappingURL=size-b630dc13.js.map

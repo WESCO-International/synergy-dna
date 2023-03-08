5ae71812ac30c4a208ddcca26f4d9f0d32d275bf
dylandepass <dylandepass@gmail.com>
2023-03-08 20:33:44 +0000

    Deploying to gh-pages from  @ 35ec637817b12e6867459d620457c769e8463cc4 🚀

---
+++ storybook/assets/color-43a22d23.js
@@ -0,0 +1,51 @@
+import{j as e}from"./jsx-runtime-ccada58e.js";import{M as t}from"./index-3820782f.js";import{T as s,a as r}from"./index-c4b7e6b1.js";import{t as o}from"./theme-eade0cca.js";import{u as d}from"./index-9ec211f2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-f8ff18ba.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-50df77d3.js";import"./chunk-XHUUYXNA-a398bfb0.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-3EFM6HRY-454626f5.js";import"./index-029c8be7.js";import"./_baseToString-30508fa8.js";import"./_getTag-5d4511eb.js";import"./isEqual-81c992b2.js";import"./index-1fe2e1f1.js";import"./uniq-efb824e2.js";import"./index-356e4a49.js";function l(i){const n=Object.assign({h1:"h1",blockquote:"blockquote",p:"p",h2:"h2"},d(),i.components);return e.jsxs(e.Fragment,{children:[e.jsx(t,{title:"Docs/Developers/Tokens/Color"}),`
+`,e.jsx(n.h1,{children:"Colors"}),`
+`,e.jsxs(n.blockquote,{children:[`
+`,e.jsx(n.p,{children:"The Synergy Design System provides CSS & mixins that is pre-configured to utilize the tokens below. They should not be used for any purpose other than the element or semantic meaning they where created for."}),`
+`]}),`
+`,e.jsx(n.h2,{children:"Primary Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:o.semantic.color.primary,prefix:"--sds-semantic-color-primary"})]}),`
+`,e.jsx(n.h2,{children:"Secondary Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:o.semantic.color.secondary,prefix:"--sds-semantic-color-secondary"})]}),`
+`,e.jsx(n.h2,{children:"Background Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:o.semantic.color.background,prefix:"--sds-semantic-color-background"})]}),`
+`,e.jsx(n.h2,{children:"Foreground Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:o.semantic.color.foreground,prefix:"--sds-semantic-color-foreground"})]}),`
+`,e.jsx(n.h2,{children:"Utility Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(r,{k:"default",value:o.semantic.color.success.default,prefix:"--sds-semantic-color-success-default"}),e.jsx(r,{k:"default",value:o.semantic.color.error.default,prefix:"--sds-semantic-color-error-default"}),e.jsx(r,{k:"default",value:o.semantic.color.warning.default,prefix:"--sds-semantic-color-warning-default"}),e.jsx(r,{k:"default",value:o.semantic.color.informative.default,prefix:"--sds-semantic-color-informative-default"})]}),`
+`,e.jsx(n.h2,{children:"Link Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:o.semantic.color.link,prefix:"--sds-semantic-color-link"})]}),`
+`,e.jsx(n.h2,{children:"Seperator Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:o.semantic.color.seperator,prefix:"--sds-semantic-color-seperator"})]}),`
+`,e.jsx(n.h2,{children:"Seperator Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:o.semantic.color.seperator,prefix:"--sds-semantic-color-seperator"})]}),`
+`,e.jsx(n.h2,{children:"Color Tokens"}),`
+`,e.jsxs("div",{className:"table token margin-top",children:[e.jsxs("div",{children:[e.jsx("div",{children:"Token"}),`
+`,e.jsx("div",{children:"Value"}),`
+`,e.jsx("div",{children:"Color"}),`
+`,e.jsx("div",{children:"Copy"})]}),e.jsx(s,{tokens:o.theme.color,prefix:"--sds-theme-color"})]})]})}function O(i={}){const{wrapper:n}=Object.assign({},d(),i.components);return n?e.jsx(n,Object.assign({},i,{children:e.jsx(l,i)})):l(i)}export{O as default};
+//# sourceMappingURL=color-43a22d23.js.map
